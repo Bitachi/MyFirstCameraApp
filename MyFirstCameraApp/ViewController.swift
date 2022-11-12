70a7e8d82bb5531f8e567b288289b7b12ca0eff8
GoTakami <funkykong888@gmail.com>
2022-11-12 13:42:41 +0900

    Initial Commit

---
+++ MyFirstCameraApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyFirstCameraApp
+//
+//  Created by 高見豪 on 2022/11/12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
